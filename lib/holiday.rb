4a2763e51226b271e6257fd71531cf0489aabc9e
Caroline Lee <carolinel2610@gmail.com>
2018-01-15 03:19:06 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -83,7 +83,7 @@ def all_supplies_in_holidays(holiday_hash)
       }
     }
   }
-  pry.binding
+  binding.pry
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
   #   Christmas: Lights, Wreath
